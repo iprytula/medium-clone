375ce38d030e5848db8d76cbf9f8bc2d2caa1bfc
Iurii Prytula <iurii.prytula@gmail.com>
2024-03-04 08:27:43 +0100

    pagination

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,22 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { combineLatest } from 'rxjs';
 import { feedActions } from 'src/app/store/actions/feed.actions';
 import { selectError, selectFeed, selectIsLoading } from 'src/app/store/reducers/feed.reducer';
+import { environment } from 'src/environments/environment.development';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { LoadingComponent } from '../loading/loading.component';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'mc-feed',
   standalone: true,
-  imports: [CommonModule, RouterModule, ErrorMessageComponent, LoadingComponent],
+  imports: [CommonModule, RouterModule, ErrorMessageComponent, LoadingComponent, PaginationComponent],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss',
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = '';
+  paginationLimit = environment.limit;
+  baseUrl = this.router.url.split('?')[0];
+  currentPage = 0;
 
   data$ = combineLatest({
     isLoading: this.store.select(selectIsLoading),
@@ -24,9 +29,17 @@ export class FeedComponent implements OnInit {
     feed: this.store.select(selectFeed)
   });
 
-  constructor(private store: Store) { }
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }));
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.currentPage = +params['page'] || 1;
+    });
   }
 }
---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UtilsService } from '../../services/utils.service';
+
+@Component({
+  selector: 'mc-pagination',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss',
+})
+export class PaginationComponent implements OnInit {
+  @Input() total = 0;
+  @Input() limit = 20;
+  @Input() currentPage = 1;
+  @Input() url = '';
+
+  pagesCount = 1;
+  pages: number[] = [];
+
+  constructor(private utilsService: UtilsService) {}
+
+  ngOnInit(): void {
+    this.pagesCount = Math.ceil(this.total / this.limit);
+    this.pages = this.pagesCount > 0 ? this.utilsService.range(1, this.pagesCount) : [];
+    console.log(this.pagesCount);
+  }
+}
